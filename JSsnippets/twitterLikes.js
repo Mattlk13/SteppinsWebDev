4a46718963d418bff91418d9f01319e0f91d477a
iitjee <iit.jee.help.me@gmail.com>
2023-04-27 22:28:41 +0530

    Update twitterLikes.js

--- JSsnippets/twitterLikes.js
+++ JSsnippets/twitterLikes.js
@@ -18,3 +18,37 @@ document.querySelectorAll("article").forEach(eachArt => {
         }
     })
  })  
+
+
+
+//task 2 - getting name, handle, postdata
+
+function hasUsernameTestId(element) {
+  const testId = element.getAttribute('data-testid')
+  if (testId && testId === 'User-Name') {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+let xarrvik = []
+document.querySelectorAll("article").forEach(eachArt => {
+    eachArt.querySelectorAll("*").forEach(u => {//get all nested, and check if it ahs aria
+       if(hasUsernameTestId(u)){
+            //after finding that elem, i thought i have to locate nested elems again but..
+            //but thankully, innerText simply clubs all inner texts anywhere within by newline
+            //so just have to parse it
+            const [name, twitterHandle, , postDate] = u.innerText.split(/\n/);
+           xarrvik.push({name, twitterHandle, postDate}) //can also push [name, handle, date] i.e in arr format, but obj{} format is nice
+            
+         //TODO: panilopani, get post's URL also. there are 3 a tags. 1 is our post url. other 2 useless userAccountURL
+         u.querySelectorAll('a').forEach(xx => console.log(xx.href)) //ToDO: u can seperate that url by "check for substr : 'status' "
+
+       
+       }
+    })
+ })
+
+
